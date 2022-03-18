51a31d425b065f86f1d3f382f030f8217d8cae2a
Georg Groenendaal <georg.groenendaal@gmail.com>
2022-03-18 13:48:39 +0100

    refactor evaluation

--- src/es_retriever.py
+++
@@ -1,9 +0,0 @@
-class ESRetriever:
-    def __init__(self, dataset_name: str = "GroNLP/ik-nlp-22_slp"):
-        self.dataset_name = dataset_name
-
-    def _setup_data(self):
-        pass
-
-    def retrieve(self, query: str, k: int):
-        pass
