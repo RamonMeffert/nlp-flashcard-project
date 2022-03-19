4f3613dcc0105a7a745e52e7c71396c25123122f
Ramon Meffert <ramon.meffert@gmail.com>
2022-03-19 15:56:38 +0100

    Tweak reader parameters

--- src/readers/dpr_reader.py
+++ src/readers/dpr_reader.py
@@ -10,7 +10,10 @@ class DprReader():
             "facebook/dpr-reader-single-nq-base"
         )
 
-    def read(self, query: str, context: Dict[str, List[str]]) -> List[Tuple]:
+    def read(self,
+             query: str,
+             context: Dict[str, List[str]],
+             num_answers=5) -> List[Tuple]:
         encoded_inputs = self._tokenizer(
             questions=query,
             titles=context['titles'],
@@ -22,6 +25,10 @@ class DprReader():
         outputs = self._model(**encoded_inputs)
 
         predicted_spans = self._tokenizer.decode_best_spans(
-            encoded_inputs, outputs)
+            encoded_inputs,
+            outputs,
+            num_spans=num_answers,
+            num_spans_per_passage=2,
+            max_answer_length=512)
 
         return predicted_spans
